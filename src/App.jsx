669228b368a47f80c1ce8b9cd8e7311b09aa652f
lukasz <luki424221@gmail.com>
2023-10-19 20:00:01 +0200

    Added header and sidebar with styles to layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Account from './pages/Account.jsx';
+import Bookings from './pages/Bookings.jsx';
+import Cabins from './pages/Cabins.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import Login from './pages/Login.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
+import Settings from './pages/Settings.jsx';
+import Users from './pages/Users.jsx';
 import GlobalStyles from './styles/GlobalStyles.js';
-import Button from './ui/Button.jsx';
-import Heading from './ui/Heading.jsx';
-import Input from './ui/Input.jsx';
-import Row from './ui/Row.jsx';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import AppLayout from './ui/AppLayout.jsx';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World!</Heading>
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => console.log('Test')}>Check in</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => console.log('Test')}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
