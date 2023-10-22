514e39e88d58e3b11b49f9c000a3ca0689c2a020
lukasz <luki424221@gmail.com>
2023-10-22 14:33:53 +0200

    Builded a reusable table, added buttons to show on modal

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,16 +1,9 @@
 import styled from 'styled-components';
+import Menus from '../../ui/Menus.jsx';
 import Spinner from '../../ui/Spinner.jsx';
+import Table from '../../ui/Table.jsx';
 import CabinRow from './CabinRow.jsx';
 import { useCabins } from './useCabins.js';
-
-const Table = styled.div`
-  border: 1px solid var(--color-grey-200);
-
-  font-size: 1.4rem;
-  background-color: var(--color-grey-0);
-  border-radius: 7px;
-  overflow: hidden;
-`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -33,19 +26,22 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table role="table">
-      <TableHeader role="row">
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </TableHeader>
-      {cabins.map(cabin => (
-        <CabinRow key={cabin.id} cabin={cabin} />
-      ))}
-    </Table>
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header role="row">
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={cabins}
+          render={cabin => <CabinRow key={cabin.id} cabin={cabin} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 
